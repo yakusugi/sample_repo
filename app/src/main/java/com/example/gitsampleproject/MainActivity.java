c21092fe5a0a58176e874fa02fb8631792eefc73
yosuke ito <yosuke_ito@aiful.co.jp>
2025-01-24 16:30:26 +0900

    変更4

--- app/src/main/java/com/example/gitsampleproject/MainActivity.java
+++ app/src/main/java/com/example/gitsampleproject/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         //test111
         //test222
+        //test444
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
