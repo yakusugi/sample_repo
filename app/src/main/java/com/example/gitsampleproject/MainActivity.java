aec25f0dd9c0ac05ab5b671215d91d12368d5e3d
yosuke ito <yosuke_ito@aiful.co.jp>
2025-01-31 13:43:12 +0900

    変更 1/31 feature-2

--- app/src/main/java/com/example/gitsampleproject/MainActivity.java
+++ app/src/main/java/com/example/gitsampleproject/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.gitsampleproject;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        String test = "great";
+        Log.d(TAG, "onCreate: " + test);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
